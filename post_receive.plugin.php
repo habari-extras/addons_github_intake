1e760e035cf9da2c01c4a7285d23984416aa89f3
mikelietz <codger+git@gmail.com>
2012-04-12 22:24:16 +0000

    Retrieve a single plugin xml file, though the URL for it isn't right yet.

--- post_receive.plugin.php
+++ post_receive.plugin.php
@@ -17,18 +17,23 @@ class PostReceive extends Plugin
 			$decoded_tree = json_decode( file_get_contents( $tree_URL, 0, null, null ) );
 			$xml_urls = array_map( function( $a ) {
 					if ( strpos( $a->path, ".plugin.xml" ) !== false ) {
-						return  $a->url . $a->path;
+						return  "{$a->url}/{$a->path}";
 					}
-				}, $decoded_tree->tree ); // NULL otherwise, need to get rid of those.
+				}, $decoded_tree->tree );
+			$xml_urls = array_filter( $xml_urls ); // remove NULLs
+			if ( count( $xml_urls ) === 1 ) {
+				$xml_url = array_pop( $xml_urls );
 
-	        Post::create( array(
-	            'title' => "incoming POST",
-	            'content' => "payload is a " . gettype( $payload ) . ", and decoded, it is a " . gettype( $decoded_payload ) . "\n" //. var_export($decoded_payload, true),
-. $tree_URL //. var_export( $decoded_tree, true ) 
-. var_export( $xml_urls, true ),
-	            'content_type' => Post::type( 'entry' ),
-	            'user_id' => $users[0]->id,
-	        ));
+		        Post::create( array(
+		            'title' => "incoming POST",
+		            'content' => "payload is a " . gettype( $payload ) . ", and decoded, it is a " . gettype( $decoded_payload ) . "\n" . $xml_url,
+		            'content_type' => Post::type( 'entry' ),
+		            'user_id' => $users[0]->id,
+		        ));
+			}
+			else {
+				// Wrong number of xml files.
+			}
 		}
 		else {
 			// Something has gone wrong with the json_decode. Do nothing, since there is nothing that can really be done.
