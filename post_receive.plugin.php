bdb1a393448f0bb68fb1700f78b1361c910c5546
mikelietz <codger+git@gmail.com>
2012-04-12 19:18:27 +0000

    Use unfiltered input. Thanks @ringmaster and @mattread.

--- post_receive.plugin.php
+++ post_receive.plugin.php
@@ -4,12 +4,12 @@ class PostReceive extends Plugin
 {
 	public function action_ajax_update( $handler ) {
 		$users = Users::get();
-		$payload = $handler->handler_vars['payload'];
+		$payload = $handler->handler_vars->raw('payload');
 		$decoded_payload = json_decode( $payload, true ); // true to return an array instead of an object.
 
         Post::create( array(
             'title' => "incoming POST",
-            'content' => "payload is a " . gettype( $payload ) . ", and decoded, it is a " . gettype( $decoded_payload ),
+            'content' => "payload is a " . gettype( $payload ) . ", and decoded, it is a " . gettype( $decoded_payload ) . "\n" . var_export($decoded_payload, true),
             'content_type' => Post::type( 'entry' ),
             'user_id' => $users[0]->id,
         ));
